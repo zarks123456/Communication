03fb8b7734bb3f49928d6e33e79e89cf5402bc1d
zarks123 <wxjncjs@163.com>
2024-08-14 22:14:49 -0400

    1. Clear the error. 2. The WinFormsApp2 is the correct one.

--- WinFormsApp2/Program.cs
+++ WinFormsApp2/Program.cs
@@ -8,13 +8,15 @@ namespace WinFormsApp2
 {
     static class Program
     {
+        private static object HighDpiMode;
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            //Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
